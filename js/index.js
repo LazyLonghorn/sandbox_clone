845464991f622dd13591a0f801664e48783d4a84
han <lkas78@naver.com>
2020-01-05 20:53:35 +0900

    main-slider ing
    Day1

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+window.onload = function(){
+    // document.getElementById('moblie-gnb').addEventListener('click', function(e){
+       
+    // });
+
+   document.getElementById('btn').addEventListener('click', function(e){
+        document.getElementById('menu').style.display = 'block';
+   });
+
+
+}
